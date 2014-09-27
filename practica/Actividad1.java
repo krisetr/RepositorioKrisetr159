d60e9ca33c063355254778d1b970557a658e9cbe
krisetr <krisetr@gmail.com>
2014-09-26 20:18:34 -0500

    Update Actividad1.java

--- practica/Actividad1.java
+++ practica/Actividad1.java
@@ -16,5 +16,6 @@ public class Actividad1 {
     
     public static void main(String args[]) {
     System.out.print("Hola este es el primer archivo subido a Github" );
+    System.out.print("Prueba de descarga" );
 }
 }
